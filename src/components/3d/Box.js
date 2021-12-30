31fce34cf50a00225aa1522cbacef145da899fea
Abhishek Asarawa <abhishek@saniot.in>
2021-12-30 15:42:30 +0530

    Roatating Box

--- src/components/3d/Box.js
+++ src/components/3d/Box.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { useSpring, a } from "@react-spring/three";
+import { useFrame } from "@react-three/fiber";
 
 const Box = () => {
+    const meshRef = useRef();
+    const [hovered, setHovered] = useState(false);
+    const [active, setActive] = useState(false);
+
+    const props = useSpring({
+        scale: active ? [1.5, 2, 3] : [1, 1, 1],
+        color: hovered ? "gray" : "red",
+    });
+
+    useFrame(() => {
+        meshRef.current.rotation.x += 0.01;
+        meshRef.current.rotation.y += 0.01;
+        meshRef.current.rotation.z += 0.01;
+    });
+
     return (
-        <mesh>
+        <a.mesh
+            ref={meshRef}
+            onClick={() => setActive((prev) => !prev)}
+            onPointerOver={() => setHovered(true)}
+            onPointerOut={() => setHovered(false)}
+            scale={props.scale}
+        >
             <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-            <meshBasicMaterial attach="material" color="red" />
-        </mesh>
+            <a.meshBasicMaterial attach="material" color={props.color} />
+        </a.mesh>
     );
 };
 
